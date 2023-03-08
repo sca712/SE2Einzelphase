934bb44a6e188d51bea03cf697ed0c811396744d
Fabian Steiner <fabianste@edu.aau.at>
2023-03-08 15:10:42 +0100

    Only prime numbers are now being displayed

--- app/src/main/java/com/example/networktest/SolutionCalculation.java
+++ app/src/main/java/com/example/networktest/SolutionCalculation.java
@@ -10,10 +10,11 @@ public class SolutionCalculation extends Thread {
     public SolutionCalculation(String matriculationNumber) {
         this.matriculationNumber = matriculationNumber;
     }
+
     public void run() {
         ArrayList<Integer> matriculationNumberArrayList = convertStringToIntArrayList(matriculationNumber);
         Collections.sort(matriculationNumberArrayList);
-        solution = matriculationNumberArrayList.toString();
+        solution = getPrimeArray(matriculationNumberArrayList).toString();
     }
 
     public ArrayList<Integer> convertStringToIntArrayList(String matriculationNumber) {
@@ -23,6 +24,34 @@ public class SolutionCalculation extends Thread {
         }
         return numberArrayList;
     }
+
+    public ArrayList<Integer> getPrimeArray(ArrayList<Integer> arrayList) {
+        ArrayList<Integer> numberArrayList = new ArrayList<>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (!isPrime(arrayList.get(i))) {
+                numberArrayList.add(arrayList.get(i));
+            }
+        }
+        return numberArrayList;
+    }
+
+    //The simplest primality test is trial division: given an
+    // input number, n, check whether it is evenly divisible by any prime number
+    // between 2 and √n (i.e. that the division leaves no remainder).
+    // If so, then n is composite. Otherwise, it is prime.
+    //Source = https://en.wikipedia.org/wiki/Primality_test
+    public static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public String getSolution() {
         return solution;
